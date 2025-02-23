e13433a26b2e084ec916128e1123eca7d786c31b
Carmelo85S <carmelo.salis85@gmail.com>
2025-02-23 11:32:53 +0100

    commented userProfile in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navigate } from "react-router-dom";
-import UserProfile from "./pages/UserProfile";
+// import UserProfile from "./pages/UserProfile";
 import { ProfileProvider } from "./hook/profileContext";
 import HomePage from "./pages/HomePage";
 import Strength from "./pages/Strength";
@@ -11,8 +11,8 @@ const App = () => {
     <ProfileProvider>
       <BrowserRouter basename="/gain-track">
         <Routes>
-          <Route path="/" element={<Navigate to="/user-profile" />} />
-          <Route path="/user-profile" element={<UserProfile />} />
+          <Route path="/" element={<Navigate to="/homepage" />} />
+          {/*<Route path="/user-profile" element={<UserProfile />} />*/}
           <Route path="/home-page" element={<HomePage />} />
           <Route path="/strength" element={<Strength />} />
         </Routes>
