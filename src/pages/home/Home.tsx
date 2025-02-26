906c846daeddb94b642e70f48941268dbb50f728
Carmelo85S <carmelo.salis85@gmail.com>
2025-02-26 19:45:55 +0100

    add heading component to home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,23 +1,19 @@
 import { useNavigate } from "react-router-dom"
 import Button from "../../components/button/Button"
+import Heading from "../../components/heading/Heading";
 
 const Home = () => {
 
     const navigate = useNavigate();
 
   return (
-    <div className="bg-main-bg bg-no-repeat bg-center bg-cover w-full h-screen overflow-hidden">
-        <div className="w-full bg-black bg-opacity-65 shadow-md rounded-lg mx-auto flex justify-center items-center flex-col font-inter">
-            <h1 className="text-main my-auto font-semibold text-3xl tracking-wide">
-                NO MORE EXCUSE
-            </h1>
-            <h2 className="ml-9 w-2/4 bg-primary-100 rounded-lg flex items-center justify-center text-black text-center my-5 mr-auto font-semibold text-2xl tracking-wide">
-                DO IT NOW
-            </h2>
+    <div className="md:hidden xs:bg-main-bg xs:bg-no-repeat xs:bg-center xs:bg-cover xs:w-full xs:h-screen xs:flex xs:flex-col xs:justify-center xs:items-center xs:overflow-hidden">
+        <div className="w-full bg-black bg-opacity-65 shadow-md rounded-lg flex flex-col items-center text-center">
+            <Heading labelH1="NO EXCUSE" labelH2="DO IT NOW" />
             <p className="w-5/6 text-main text-justify">
                 Achieve your fitness goals and track your progress effortlessly. Stay consistent, push your limits, and see real results with every workout.
             </p>
-            <div className="w-4/5 my-2 flex justify-between items-center">
+            <div className="w-3/5 my-2 flex justify-between items-center">
                 <Button 
                     label="Sign In" 
                     onClick={() => navigate("/signin")}
